0ed82ab31d473d2a270a28d9bd08220dd5828664
tuz1k <bohodurk@gmail.com>
2017-11-24 12:16:09 -0800

    Update footer.php

--- www/html/layouts/footer.php
+++ www/html/layouts/footer.php
@@ -1,4 +1,4 @@
 <div class="footer">
-  <h5><a href="/tos">Terms & Conditions</a> <span style="font-size:6px;">•</span> <a href="http://steamcommunity.com/groups/tuz1k.com/discussions/0/1474221865183316300/" target="_blank">Partnership</a> <span style="font-size:6px;">•</span> <a href="" target="_blank">Contact</a></h5>
-  <h6>&copy; 2017 Nanoflip</h6>
+  <h5><a href="/tos">Terms & Conditions</a> <span style="font-size:6px;">•</span> <a href="http://steamcommunity.com/id/tuz1k.com/" target="_blank">Partnership</a> <span style="font-size:6px;">•</span> <a href="" target="_blank">Contact</a></h5>
+  <h6>&copy; 2017 tuz1k</h6>
 </div>
