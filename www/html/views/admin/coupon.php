4036134ad6b3d48e39d02c0b94c65db3f4b8638c
tuz1k <bohodurk@gmail.com>
2018-01-07 20:14:33 -0800

    Update coupon.php

--- www/html/views/admin/coupon.php
+++ www/html/views/admin/coupon.php
@@ -1,8 +1,8 @@
 <?php
-if(getPost('value') && getPost('uses') && getPost('value') > 0 && getPost('uses') > 0) {
-  //createCoupon(getPost('value'), getPost('uses'));
+if (getPost('value') && getPost('uses') && getPost('value') > 0 && getPost('uses') > 0) {
+  createCoupon(getPost('value'), getPost('uses'));
 }
-if($_params[0] == 'new') { ?>
+if (@$_params[0] == 'new') { ?>
   <h1 class="title">Create Code</h1>
   <form method="post">
     <input type="number" name="value" step="any" min="0" placeholder="Code Value" />
@@ -11,17 +11,18 @@ if($_params[0] == 'new') { ?>
     <br />
     <input type="submit" class="btn btn-primary green" value="Create Code" />
   </form>
-<?php } else {
+<?php
+} else {
   $results = Query('SELECT `coupon`.*, COUNT(`users_coupons`.`id`) AS "count" FROM `coupon` LEFT JOIN `users_coupons` ON `couponid` = `coupon`.`id`', null, '`coupon`.`id`', 'DESC', null, '`coupon`.`id`');
   $table = '';
-  foreach($results as $result) {
+  foreach ($results as $result) {
     $rem = $result['uses'] - $result['count'];
     $table .= '<tr>
-      <td>'.$result['id'].'</td>
-      <td>'.$result['code'].'</td>
-      <td>$'.prettyBalance($result['value']).'</td>
-      <td>'.$rem.'</td>
-      <td>'.$result['uses'].'</td>
+      <td>' . $result['id'] . '</td>
+      <td>' . $result['code'] . '</td>
+      <td>$' . prettyBalance($result['value']) . '</td>
+      <td>' . $rem . '</td>
+      <td>' . $result['uses'] . '</td>
     </tr>';
   } ?>
   <h1 class="title">View Codes</h1>
@@ -38,4 +39,5 @@ if($_params[0] == 'new') { ?>
     <tbody><?= $table ?></tbody>
   </table>
   <a class="btn btn-primary green" href="/admin/coupon/new">Create Code</a>
-<?php } ?>
+<?php
+} ?>
